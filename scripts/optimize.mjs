3d2770158614af4a7443de08657cb2d42feccaea
Jerry Wang <x.jerry.wang@gmail.com>
2021-09-21 10:28:15 +0800

    chore: update option

--- scripts/optimize.mjs
+++ scripts/optimize.mjs
@@ -44,8 +44,24 @@ async function resolveImage(file) {
   await image.preprocess({})
 
   await image.encode({
-    //an empty object means 'use default settings'
-    mozjpeg: {}
+    mozjpeg: {
+      quality: 90,
+      baseline: false,
+      arithmetic: false,
+      progressive: true,
+      optimize_coding: true,
+      smoothing: 0,
+      color_space: 3,
+      quant_table: 3,
+      trellis_multipass: false,
+      trellis_opt_zero: false,
+      trellis_opt_table: false,
+      trellis_loops: 1,
+      auto_subsample: true,
+      chroma_subsample: 2,
+      separate_chroma_quality: false,
+      chroma_quality: 75
+    }
   })
 
   const rawEncodedImage = (await image.encodedWith.mozjpeg).binary
