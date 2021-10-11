421bbcdeea06069e1819cd3b2137b6b1850b16e0
Jerry Wang <x.jerry.wang@gmail.com>
2021-10-11 08:11:04 +0800

    feat: use webp format

--- scripts/optimize.mjs
+++ scripts/optimize.mjs
@@ -25,7 +25,8 @@ async function main() {
 
       const newFile = await resolveImage(file)
 
-      const outputImgPath = r('images', p)
+      const outputImgPath = r('images', p.replace(/\.\w+$/, '') + '.webp')
+
       await fs.writeFile(outputImgPath, newFile)
 
       const outputStat = await fs.stat(outputImgPath)
@@ -55,27 +56,38 @@ async function resolveImage(file) {
   await image.preprocess({})
 
   await image.encode({
-    mozjpeg: {
+    webp: {
       quality: 75,
-      baseline: false,
-      arithmetic: false,
-      progressive: true,
-      optimize_coding: true,
-      smoothing: 0,
-      color_space: 3,
-      quant_table: 3,
-      trellis_multipass: false,
-      trellis_opt_zero: false,
-      trellis_opt_table: false,
-      trellis_loops: 1,
-      auto_subsample: true,
-      chroma_subsample: 2,
-      separate_chroma_quality: false,
-      chroma_quality: 75
+      target_size: 0,
+      target_PSNR: 0,
+      method: 4,
+      sns_strength: 50,
+      filter_strength: 60,
+      filter_sharpness: 0,
+      filter_type: 1,
+      partitions: 0,
+      segments: 4,
+      pass: 1,
+      show_compressed: 0,
+      preprocessing: 0,
+      autofilter: 0,
+      partition_limit: 0,
+      alpha_compression: 1,
+      alpha_filtering: 1,
+      alpha_quality: 100,
+      lossless: 0,
+      exact: 0,
+      image_hint: 0,
+      emulate_jpeg_size: 0,
+      thread_level: 0,
+      low_memory: 0,
+      near_lossless: 100,
+      use_delta_palette: 0,
+      use_sharp_yuv: 0
     }
   })
 
-  const rawEncodedImage = (await image.encodedWith.mozjpeg).binary
+  const rawEncodedImage = (await image.encodedWith.webp).binary
 
   return rawEncodedImage
 }
